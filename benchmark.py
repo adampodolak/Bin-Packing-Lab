269cb7733594b447fb7a103ae16be903eb0ad544
adampodolak <podola2@mcmaster.ca>
2022-11-06 18:26:11 -0500

    t4 benchmarks

--- benchmark.py
+++ benchmark.py
@@ -1,8 +1,8 @@
 import pyperf
 from os import listdir
 from os.path import isfile, join, basename
-from macpacking.algorithms.online import NextFit as Nf_online, BestFit, WorstFit, TerriblePacker, FirstFit
-from macpacking.algorithms.offline import NextFit as Nf_offline, FirstFitDecreasing, BestFitDecreasing, WorstFitDecreasing 
+from macpacking.algorithms.online import NextFit as Nf_online, BestFit, WorstFit, TerriblePacker, FirstFit, RefinedFirstFit
+from macpacking.algorithms.offline import NextFit as Nf_offline, FirstFitDecreasing, BestFitDecreasing, WorstFitDecreasing, RefinedFirstFitDecreasing
 from macpacking.algorithms.baseline import BenMaier
 from macpacking.reader import BinppReader
 
@@ -21,11 +21,17 @@ class Benchmark:
         'ff-offline': FirstFitDecreasing(),
         'baseline': BenMaier()
     }
+
+    t4_algos = {
+        'rff-online': RefinedFirstFit(),
+        'rff-offline': RefinedFirstFitDecreasing()
+    }
     
     CASES = './_datasets/binpp/N4C2W2'
 
     def __init__(self) -> None:
         self.algos
+        self.t4_algos
         self.CASES
 
 
@@ -44,13 +50,26 @@ class Benchmark:
                     runner.bench_func(name + '-' + a, self.algos[a], data_offline)
                 else:
                     runner.bench_func(name + '-' + a, self.algos[a], data_online)
+    
+    def run_t4_bench(self, cases: list[str]):
+        runner = pyperf.Runner()
+        for case in cases:
+            name = basename(case)
+            data_online = BinppReader(case).online()
+            data_offline = BinppReader(case).offline()
+            for a in self.t4_algos:
+                if a == 'rff-online':
+                    runner.bench_func(name + '-' + a, self.t4_algos[a], data_online)
+                else:
+                    runner.bench_func(name + '-' + a, self.t4_algos[a], data_offline)
 
 def main():
     '''Example of benchmark code'''
     benchmark = Benchmark()
     cases = benchmark.CASES
     cases = benchmark.list_case_files(cases)
-    benchmark.run_bench(cases)
+    #benchmark.run_bench(cases)
+    benchmark.run_t4_bench(cases)
 
 if __name__ == "__main__":
     main()
